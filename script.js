36d7873b868b4dc41251bb4996365f6df1197dcc
Yuri <101836890+YuriBrag@users.noreply.github.com>
2022-03-21 19:33:47 -0300

    adicionei Certificard ver 1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+  //   var imgGitWhite = document.getElementById("imgGit")
+
+  //   imgGitWhite.innerHTML = "<img src='https://img1.gratispng.com/20180807/skj/kisspng-github-pages-logo-repository-fork-github-logo-1-magentys-5b69de714d60d9.937126221533664881317.jpg' alt='' class='redes-sociais' id='imgGit'>"
+}
